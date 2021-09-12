ed9fb64a9e7c42900122e0539350890255746268
bayjohnpaul08 <bay.johnpaul08@gmail.com>
2021-09-12 23:44:43 +0800

    First api commit

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<?php
+
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../includes/DbConnect.php';
+ 
+$app = AppFactory::create();
+$app->setBasePath("/MyApi/public");
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+
+    $db = new DbConnect;
+
+    if($db->connect() != null){
+        echo 'Connection Successful';
+    }
+
+    return $response;
+});
+
+$app->run();
